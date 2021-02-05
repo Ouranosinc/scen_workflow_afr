276a9fddf151c87f29033800b90f1d588370031a
yrouranos <rousseau.yannick@ouranos.ca>
2021-02-04 21:47:48 -0500

    Replacing nan with lat-lon interpolated values when calculating 'rainstart' and 'rainend' indices.

--- indices.py
+++ indices.py
@@ -1127,11 +1127,11 @@ def rain_qty(da_pr: xr.DataArray, da_rainstart: xr.DataArray, da_rainend: xr.Dat
             da_end = da_rainend[np.array(years_idx) == str(y)].squeeze()
 
         # Condition.
-        da_cond = (((da_start <= da_end) & ((doy < da_start) | (doy > da_end))) |
-                   ((da_start > da_end) & ((doy > da_start) & (doy < da_end))))
+        da_cond = (((da_start <= da_end) & (doy >= da_start) & (doy <= da_end)) |
+                   ((da_start > da_end) & ((doy <= da_start) | (doy >= da_end))))
 
         # Discard values.
-        da_pr[t] *= da_cond
+        da_pr[t] = da_pr[t] * da_cond.astype(float)
 
     # Sum by year.
     with warnings.catch_warnings():
