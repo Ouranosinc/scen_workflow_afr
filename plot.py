f6b7732a3f17a95672ce94a5a51834a0bb0fc0c1
yrouranos <rousseau.yannick@ouranos.ca>
2021-06-04 22:10:13 -0400

    Improved color scales in heat maps.

--- plot.py
+++ plot.py
@@ -838,7 +838,11 @@ def plot_heatmap(da: xr.DataArray, stn: str, var_or_idx_code: str, grid_x: [floa
                 for i in range(n_cluster + 1):
                     tick = i / float(n_cluster) * (vmax - vmin) + vmin
                     ticks.append(tick)
-                    str_ticks.append(("{:." + str(n_dec) + "f}").format(str(round(tick, n_dec))))
+                    str_tick = str(round(tick, n_dec))
+                    if n_dec == 0:
+                        str_ticks.append(str_tick)
+                    else:
+                        str_ticks.append(str("{:." + str(n_dec) + "f}").format(str_tick))
 
                     # Two consecutive rounded tick labels are equal.
                     if i > 0:
